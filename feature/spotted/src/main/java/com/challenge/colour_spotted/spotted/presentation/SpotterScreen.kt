9f68e33e0b2fb9d5adce254736a1d07aaf7dd33f
lorenzo.niero <lorenzo.niero@milkmantechnologies.com>
2024-12-02 23:42:53 +0100

    added button on bottom

--- feature/spotted/src/main/java/com/challenge/colour_spotted/spotted/presentation/SpotterScreen.kt
+++ feature/spotted/src/main/java/com/challenge/colour_spotted/spotted/presentation/SpotterScreen.kt
@@ -1,21 +1,26 @@
 package com.challenge.colour_spotted.spotted.presentation
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.challenge.colour_spotter.ui.component.TopBar
 import com.challenge.colour_spotter.ui.theme.ColourSpotterTheme
+import com.challenge.colour_spotter.ui.R as R_UI
 
 @Composable
 fun SpotterScreen (
@@ -33,7 +38,7 @@ private fun ListContent(
     Scaffold(
         topBar = {
             TopBar(
-                title = "NAME APP",//stringResource(id = R.string.app_name),
+                title = stringResource(id = R_UI.string.app_name),
                 onBackClick = null,
                 actions = {
                     IconButton(onClick = {
@@ -47,11 +52,20 @@ private fun ListContent(
                 }
             )
         }) { innerPadding ->
-        Column (
+        Box (
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
+
+            Button(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                onClick = {
+                    //TODO
+                }
+            ) {
+                Text(text = "Start")
+            }
 
         }
     }
@@ -60,7 +74,7 @@ private fun ListContent(
 
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
-private fun ListScreenPreview() {
+private fun SpotterScreenPreview() {
     ColourSpotterTheme {
         ListContent()
     }
